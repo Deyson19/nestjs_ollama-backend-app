5a33e147dc8363a1a2c100cfa9fe6a1e749f5af5
Deyson19 <deiandresvent@hotmail>
2025-07-14 12:44:10 -0500

    ✨ refactor(main): configurar puerto de escucha
    - El puerto se establece por variable de entorno o por defecto.
    - Se agrega registro del inicio del servidor.
    - Se usa el puerto 5000 por defecto.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main');
   app.setGlobalPrefix('api/v1');
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 5000;
+  await app.listen(port, () => {
+    logger.log(`Server running on port ${port}`);
+  });
 }
 bootstrap();

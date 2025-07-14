d60ae1eb7cb3b1750c82925f0f63dfccbbea364f
Deyson19 <deiandresvent@hotmail>
2025-07-14 13:47:34 -0500

    🐛 fix(ollama): corrige manejo de respuestas del endpoint
    - Resuelve el error de respuesta nula.
    - Añade código de estado HTTP 200.
    - Elimina la propiedad "request" de la respuesta.

--- src/ollama/ollama.controller.ts
+++ src/ollama/ollama.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { OllamaService } from './ollama.service';
 
 @Controller('ollama')
@@ -6,11 +6,12 @@ export class OllamaController {
   constructor(private readonly ollamaService: OllamaService) {}
 
   @Post()
+  @HttpCode(HttpStatus.OK)
   public async generateResponse(@Body() body: { prompt: string }) {
     const resp = await this.ollamaService.generateResponse(body.prompt);
+
     return {
       success: resp !== null,
-      request: body.prompt,
       response: resp,
     };
   }

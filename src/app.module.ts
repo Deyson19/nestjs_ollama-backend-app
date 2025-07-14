38536173f32441cf795af1a8da334ac2564aadcb
Deyson19 <deiandresvent@hotmail>
2025-07-14 12:45:18 -0500

    📦 build(app): añade configuración de variables de entorno
    - Configura el módulo ConfigModule para cargar variables de entorno.
    - Carga el archivo .env para el desarrollo local.
    - Permite el acceso a variables de entorno en toda la aplicación.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OllamaModule } from './ollama/ollama.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [OllamaModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    OllamaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

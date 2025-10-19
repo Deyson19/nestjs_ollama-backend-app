a4b4fa17b6fcf9283384842d95afb3f49eda8c92
Deyson19 <deysonvente@gmail.com>
2025-10-19 10:17:53 -0500

    ✨ feat(main): agregar documentación swagger a la api
    - Agregar configuración de Swagger para la documentación de la API.
    - Configurar títulos, descripciones y versiones para la documentación.
    - Habilitar la visualización de la documentación en la ruta /api.
    - Añadir etiquetas para organizar los endpoints en la documentación.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Main');
   app.setGlobalPrefix('api/v1');
   const port = process.env.PORT || 5000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Ollama API')
+    .setDescription('My Ollama Local API Server')
+    .setVersion('1.0')
+    .addTag('App')
+    .addTag('Ollama')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   app.enableCors();
   await app.listen(port, () => {
     logger.log(`Server running on port ${port}`);
